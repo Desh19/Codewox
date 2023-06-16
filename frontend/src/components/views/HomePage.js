cde15221c113a1f38f71ae73f236e0ba7dd47225
Deshan Rajapaksha <98569506+Desh19@users.noreply.github.com>
2023-06-16 13:20:23 +0530

    changes
    changes

---
+++ frontend/src/components/views/HomePage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './Header';
+import Footer from './Footer';
+import '../css/homepage.css';
+
+const HomePage = () => {
+
+  return (
+    <div>
+      <Header />
+      <br></br>
+      hello
+      <Footer />
+
+    </div>  
+  )
+}
+
+
+export default HomePage
